137d89c3fee03ca572647fb07c86b72c2435e586
齋藤健悟 <k.saitoh0526@gmail.com>
2019-03-02 19:39:54 +0900

    show WebView at the Book surface tracking

--- WalkingWebView/ViewController.swift
+++ WalkingWebView/ViewController.swift
@@ -12,30 +12,34 @@ import ARKit
 class ViewController: UIViewController {
     
     var sceneView: ARSCNView!
+    var webView: UIWebView!
     
     override func loadView() {
         super.loadView()
         
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        webView = UIWebView(frame: CGRect(x: 0, y: 0, width: 640, height: 480))
+        let request = URLRequest(url: URL(string: "http://www.amazon.com")!)
+        webView.loadRequest(request)
         
         sceneView = ARSCNView(frame: .zero)
-        sceneView.addGestureRecognizer(gesture)
-        let configuration = ARWorldTrackingConfiguration()
+        sceneView.delegate = self
+        
+        let configuration = ARImageTrackingConfiguration()
+        let images = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources",
+                                                      bundle: nil)!
+        configuration.trackingImages = images
         sceneView.session.run(configuration)
         view.addSubview(sceneView)
     }
     
-    @objc private func tapped() {
+    private func makeNode(anchor: ARImageAnchor) -> SCNNode? {
         guard let currentFrame = self.sceneView.session.currentFrame else {
-            return
+            return nil
         }
-        let uiWebView = UIWebView(frame: CGRect(x: 0, y: 0, width: 640, height: 480))
-        let request = URLRequest(url: URL(string: "http://www.amazon.com")!)
-        
-        uiWebView.loadRequest(request)
-        
-        let plane = SCNPlane(width: 1.0, height: 0.75)
-        plane.firstMaterial?.diffuse.contents = uiWebView
+        let size = anchor.referenceImage.physicalSize
+        // 同じサイズの平面ジオメトリを作成
+        let plane = SCNPlane(width: size.width, height: size.height)
+        plane.firstMaterial?.diffuse.contents = webView
         plane.firstMaterial?.isDoubleSided = true
         
         let node = SCNNode(geometry: plane)
@@ -43,9 +47,11 @@ class ViewController: UIViewController {
         var translation = matrix_identity_float4x4
         translation.columns.3.z = -1.0
         
-        node.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
-        node.eulerAngles = SCNVector3(0,0,0)
-        self.sceneView.scene.rootNode.addChildNode(node)
+//        node.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        node.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+//        node.eulerAngles = SCNVector3(0,0,0)
+        
+        return node
     }
     
     override func viewWillLayoutSubviews() {
@@ -54,3 +60,64 @@ class ViewController: UIViewController {
         sceneView.frame = frame
     }
 }
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        print("anchor:\(anchor), node: \(node), node geometry: \(String(describing: node.geometry))")
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        
+        // 仮想コンテンツを設置
+        let madeNode = makeNode(anchor: imageAnchor)
+        DispatchQueue.main.async(execute: {
+            node.addChildNode(madeNode!)
+        })
+    }
+}
+
+extension ARImageAnchor {
+    // 画像アンカーと同じサイズの平面ジオメトリを持つノードを追加する
+    func addPlaneNode(on node: SCNNode, color: UIColor) {
+        // 物理サイズを取得
+        let size = referenceImage.physicalSize
+        
+        // 同じサイズの平面ジオメトリを作成
+        let geometry = SCNPlane(width: size.width, height: size.height)
+        geometry.materials.first?.diffuse.contents = color
+        
+        // 平面ジオメトリを持つノードを作成
+        let planeNode = SCNNode(geometry: geometry)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+        
+        DispatchQueue.main.async(execute: {
+            node.addChildNode(planeNode)
+        })
+    }
+    
+    func findPlaneNode(on node: SCNNode) -> SCNNode? {
+        for childNode in node.childNodes {
+            if childNode.geometry as? SCNPlane != nil {
+                return childNode
+            }
+        }
+        return nil
+    }
+    
+    func updatePlaneNode(on node: SCNNode) {
+        let size = referenceImage.physicalSize
+        DispatchQueue.main.async(execute: {
+            guard let planeNode = self.findPlaneNode(on: node) else { return }
+            guard let plane = planeNode.geometry as? SCNPlane else { fatalError() }
+            // 平面ジオメトリのサイズを更新
+            plane.width = size.width
+            plane.height = size.height
+        })
+    }
+    
+    func removePlaneNode(on node: SCNNode) {
+        DispatchQueue.main.async(execute: {
+            guard let planeNode = self.findPlaneNode(on: node) else { return }
+            planeNode.removeFromParentNode()
+        })
+    }
+}
+
